ca6a42f188e259a26b62dce36e7c7c720ba6bd09
Deepak Verma <deepak.23.verma@gmail.com>
2013-10-01 07:55:51 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CrowdfunderDeepak::Application.config.secret_key_base = '9c4dd66cfe118b14885efef7586aa14de44cfe893fb814c6cc104cf3c6d1fb43f1cfa5761a7f6a9921c788c2c0b88c1e8b7b1110359ed85e6f7337a9c90fd1b8'
